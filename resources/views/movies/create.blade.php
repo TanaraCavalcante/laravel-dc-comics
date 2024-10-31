44237085da8e1331358df1106e5bb1b784109957
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-10-31 17:47:47 +0100

    input value "old"

--- resources/views/movies/create.blade.php
+++ resources/views/movies/create.blade.php
@@ -26,65 +26,65 @@
                     <div class="mb-3 row">
                       <label for="movie-poster" class="col-sm-3 col-form-label">Addres to the poster</label>
                       <div class="col-sm-8">
-                      <input type="text" class="form-control" id="movie-poster" name="poster">
+                      <input type="text" class="form-control" id="movie-poster" name="poster" value="{{ old('poster')}}">
                      </div>
                     </div>
                     <div class="mb-3 row">
                         <label for="movie-original_title" class="col-sm-3 col-form-label">Original Title</label>
                         <div class="col-sm-8">
-                        <input type="text" class="form-control" id="movie-original_title" name="original_title">
+                        <input type="text" class="form-control" id="movie-original_title" name="original_title" value="{{ old('original_title')}}">
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label for="movie-title" class="col-sm-3 col-form-label">Italian Title</label>
                         <div class="col-sm-8">
-                        <input type="text" class="form-control" id="movie-title" name="title">
+                        <input type="text" class="form-control" id="movie-title" name="title" value="{{ old('title')}}">
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label for="movie-description" class="col-sm-3 col-form-label">Description</label>
                         <div class="col-sm-8">
-                        <input type="text" class="form-control" id="movie-description" name="description">
+                        <input type="text" class="form-control" id="movie-description" name="description" value="{{ old('description')}}">
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label for="movie-genre" class="col-sm-3 col-form-label">Genre</label>
                         <div class="col-sm-8">
-                        <input type="text" class="form-control" id="movie-genre" name="genre">
+                        <input type="text" class="form-control" id="movie-genre" name="genre" value="{{ old('genre')}}">
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label for="movie-actors" class="col-sm-3 col-form-label">Actors</label>
                         <div class="col-sm-8">
-                        <input type="text" class="form-control" id="movie-actors" name="actors">
+                        <input type="text" class="form-control" id="movie-actors" name="actors" value="{{ old('actors')}}">
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label for="movie-director" class="col-sm-3 col-form-label">Direction</label>
                         <div class="col-sm-8">
-                        <input type="text" class="form-control" id="movie-director" name="director">
+                        <input type="text" class="form-control" id="movie-director" name="director" value="{{ old('director')}}">
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label for="movie-duration_minutes" class="col-sm-3 col-form-label">Duraction in minutes</label>
                         <div class="col-sm-3">
-                        <input type="number" class="form-control" id="movie-duration_minutes" name="duration_minutes">
+                        <input type="number" class="form-control" id="movie-duration_minutes" name="duration_minutes" value="{{ old('duration_minutes')}}">
                         </div>
                         <label for="movie-year" class="col-sm-2 col-form-label text-end">Year</label>
                         <div class="col-sm-3">
-                        <input type="number" class="form-control " id="movie-year" name="year">
+                        <input type="number" class="form-control " id="movie-year" name="year" value="{{ old('year')}}">
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label for="movie-country" class="col-sm-3 col-form-label">Country</label>
                         <div class="col-sm-8">
-                        <input type="text" class="form-control" id="movie-country" name="country">
+                        <input type="text" class="form-control" id="movie-country" name="country" value="{{ old('country')}}">
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label for="movie-studio" class="col-sm-3 col-form-label">Studio</label>
                         <div class="col-sm-8">
-                        <input type="text" class="form-control" id="movie-studio" name="studio">
+                        <input type="text" class="form-control" id="movie-studio" name="studio" value="{{ old('studio')}}">
                         </div>
                     </div>
                     <div class="col-12 col-md-11 text-end">
