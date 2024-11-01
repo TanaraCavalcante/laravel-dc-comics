9676acaa93787c74ff9d47a624be4f6e9f6ca6eb
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-11-01 18:32:14 +0100

    style in home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,12 +5,11 @@
 @section("main-content")
 <section id="home">
     <div class="container" >
-        <div class="row justify-content-center py-3">
-            <h2 class="col-12 col-md-8 mb-5" >Movie-Heros Library</h2>
-            <div>
-
+        <div class="row justify-content-center mt-1 p-1">
+            <h1 class="col-10 col-md-8 mb-3 text-center" >Hero-movies Library</h1>
+            <div class="text-center">
+                <img src="{{ asset('images/cinehero.jpeg') }}" alt="Heros">
             </div>
-
         </div>
     </div>
 </section>
